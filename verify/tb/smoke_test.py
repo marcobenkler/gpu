2cfe58540353129ba401ca4b3ec82ad3c71334ee
Marco Benkler <marcobenkler@users.noreply.github.com>
2026-06-23 00:31:11 +0200

    Improve smoke_test by adding a class wrapper for wsched_entry and refactor test program

--- verify/tb/smoke_test.py
+++ verify/tb/smoke_test.py
@@ -1,5 +1,5 @@
 import cocotb
-import struct
+import numpy as np
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, ClockCycles
 
@@ -27,17 +27,27 @@ def addi(rd, rs1, imm):
 class WarpCtx:
     def __init__(self, val):
         v = int(val)
-        self.pc = (v >> LANE_CNT) & 0xFFFFFFFF
+        self.pc   = (v >> LANE_CNT) & 0xFFFFFFFF
         self.amsk = v & ((1 << LANE_CNT) - 1)
+
+class WarpEntry:
+    def __init__(self, val):
+        v = int(val)
+        rank_bits       = int(np.log2(WARP_CNT))
+        self.state      = (v >> (rank_bits + 32)) & 0x7
+        self.age_rank   = (v >> 32) & ((1 << rank_bits) - 1)
+        self.scoreboard = v & 0xFFFFFFFF
+
+def add_simple():
+    return [
+        addi(1, 0, 5),
+        addi(2, 0, 10),
+        add (3, 1, 2)
+    ]
 
 @cocotb.test()
 async def smoke_test(dut): #dut defined in makefile
-    program = [
-        addi(1, 0, 5),
-        addi(2, 0, 10),
-        add (3, 1, 2) #scoreboard has to stall add, cause rd 2 is rs2
-        #add (3, 1, 2)
-    ]
+    program = add_simple()
 
     for addr, instr in enumerate(program):
         dut.u_instr_mem.instr_mem[addr].value = instr
@@ -53,9 +63,9 @@ async def smoke_test(dut): #dut defined in makefile
         await RisingEdge(dut.clk)
 
         ctx    = WarpCtx(dut.u_wctx.ctx_tbl[warp_num].value)
-        state = dut.u_wstate_ctrl.wsched_entry[warp_num].value
+        entry  = WarpEntry(dut.u_wstate_ctrl.wsched_entry[warp_num].value)
 
-        cocotb.log.info(f"cyc={cyc}, pc={ctx.pc}, state={state}")
+        cocotb.log.info(f"cyc={cyc}, pc={ctx.pc}, state={entry.state}")
 
     x3 = dut.u_gpr.reg_mem[warp_num + 3].value
     assert x3 == 15, f"x3 expected 15, got {x3}"
